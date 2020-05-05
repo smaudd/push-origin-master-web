8b59790e73597caa84a01bcf89515e8a6b65ab1b
Santiago Marcano <smarcanoweb@gmail.com>
2020-05-05 21:19:35 +0200

    Gulp build command

--- gulpfile.esm.js/index.js
+++ gulpfile.esm.js/index.js
@@ -1,10 +1,11 @@
-import { parallel, watch } from 'gulp'
+import { parallel, watch, task, series } from 'gulp'
 import bs from 'browser-sync'
 import html from './html'
 import assets from './assets'
 import minimist from 'minimist'
 import scripts from './scripts'
 import css from './css'
+import del from 'del'
 
 const argv = minimist(process.argv.slice(2))
 const reload = bs.reload
@@ -20,6 +21,7 @@ function server() {
             open: false,
         })
     }
+    return
 }
 
 function recompile(path) {
@@ -45,11 +47,19 @@ function recompile(path) {
     reload()
 }
 
-const sourceWatcher = watch(['./src/', './content'])
-sourceWatcher.on('change', recompile)
-sourceWatcher.on('add', recompile)
-sourceWatcher.on('unlink', recompile)
-sourceWatcher.on('addDir', recompile)
-sourceWatcher.on('unlinkDir', recompile)
+if (argv.dev) {
+    const sourceWatcher = watch(['./src/', './content'])
+    sourceWatcher.on('change', recompile)
+    sourceWatcher.on('add', recompile)
+    sourceWatcher.on('unlink', recompile)
+    sourceWatcher.on('addDir', recompile)
+    sourceWatcher.on('unlinkDir', recompile)
+}
+
+function clean() {
+    return del(['public'])
+}
+
+task('build', series(clean, parallel(html, scripts, css, assets)))
 
 export default parallel(server, html, scripts, css, assets)
